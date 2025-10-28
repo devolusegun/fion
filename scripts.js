d2f509a0622acdfbb3bc03382cca8e9b629e2c37
Dev Olusegun <12515139o@gmail.com>
2025-10-28 09:01:01 +0100

    initial fion's source

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Simple interactivity: alert on trial button click
+document.getElementById('trial-btn').addEventListener('click', () => {
+  alert('Thank you for your interest! Start your 7-day free trial now.');
+});
